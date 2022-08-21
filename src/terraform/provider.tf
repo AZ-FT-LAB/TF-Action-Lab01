0d9c2b9695b950f933b38dc898724b9c0d2163d0
nalindapw <110644294+nalindapw@users.noreply.github.com>
2022-08-21 21:07:25 +1200

    add them all

--- src/terraform/provider.tf
+++ src/terraform/provider.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "cloud-shell-storage-southeastasia"
     storage_account_name = "cs110032002139b694c"
     container_name       = "tfstate"
-    key                  = "lab02.terraform.tfstate"
+    key                  = "demo.terraform.tfstate"
   }
 }
